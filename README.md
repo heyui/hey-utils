9f5e4bc4f44eab6f6bcfaa704b62e7a6eaf27f17
vvpvvp <alias8516@sina.com.cn>
2017-03-28 18:45:31 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,119 @@
 # hey-utils
-公用方法
+
+## 判断类型
+
+### isObject  
+### isArray  
+### isDate  
+### isNumber  
+### isString  
+### isBoolean  
+### isFunction  
+### isNull  
+### isPlainObject  
+
+
+## 对象复制，合并，赋值
+
+### extend  
+用一个或多个其他对象来扩展一个对象，返回被扩展的对象。  
+如果第一个参数设置为true，则返回一个深层次的副本，递归地复制找到的任何对象。  
+否则的话，副本会与原对象共享结构。 未定义的属性将不会被复制，然而从对象的原型继承的属性将会被复制。  
+例：<code>Utils.extend({},{a:1},{b:2});</code>
+```js
+{
+  a:1,
+  b:2
+}
+```
+
+### deepFreeze
+深度冻结对象
+Object.freeze的深度实现
+例：<code>Utils.freeze({a:1});</code>
+
+### deepCopy
+深度拷贝对象  
+Object.assign的深度实现
+例：<code>Utils.assign({a:1});</code>
+
+### valueForKeypath
+获取深度path的对象值
+例：<code>Utils.valueForKeypath(obj,'a.b.c.d');</code>
+
+### setValueForKeypath
+对深度path的对象赋值
+例：<code>Utils.setValueForKeypath(obj,'a.b.c.d',1);</code>
+
+
+## toArray, toObject
+
+### toArray
+将object转换成array. 
+<code>toArray(object,keyName,valueName)</code> 
+例：  
+```js
+  Utils.toArray({a:1,b:1},'key','value');
+
+  //结果
+  [{
+    key:'a',
+    value:1
+  },{
+    key:'b',
+    value:1
+  }]
+
+
+  Utils.toArray({a:{b:2,d:4},b:{c:2,e:5}},'key');
+
+  //结果
+  [{
+    key:'a',
+    b:2,
+    d:4
+  },{
+    key:'b',
+    c:2,
+    e:5
+  }]
+
+```
+
+### toObject
+将array转换成object.  
+例：  
+```js
+  Utils.toObject([{id:'a',b:2},{id:'b',b:2}],'id');
+
+  //结果
+  {
+    a:{
+      id:'a',
+      b:2
+    },
+    b:{
+      id:'b',
+      b:2
+    }
+  }
+
+
+  Utils.toObject([{id:'a',b:2},{id:'b',b:2}],'id',true);
+
+  //结果
+  {
+    a:{
+      id:'a',
+      b:2,
+      count:0
+    },
+    b:{
+      id:'b',
+      b:2,
+      count:1
+    }
+  }
+```
+
+
---
+++ utils.js
@@ -0,0 +1,190 @@
+(function(global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    global.heyUtils = factory();
+}((typeof window == 'object' ? window : typeof global == 'object' ? global : this), function() {
+  "use strict";
+  const heyUtils = {
+    isObject: function(input) {
+      return Object.prototype.toString.call(input) === '[object Object]';
+    },
+    isArray: function(input) {
+      return input instanceof Array || Object.prototype.toString.call(input) === '[object Array]';
+    },
+    isDate: function(input) {
+      return input instanceof Date || Object.prototype.toString.call(input) === '[object Date]';
+    },
+    isNumber: function(input) {
+      return input instanceof Number || Object.prototype.toString.call(input) === '[object Number]';
+    },
+    isString: function(input) {
+      return input instanceof String || Object.prototype.toString.call(input) === '[object String]';
+    },
+    isBoolean: function(input) {
+      return typeof input == 'boolean';
+    },
+    isFunction: function(input) {
+      return typeof input == 'function';
+    },
+    isNull: function(input) {
+      return input === undefined || input === null;
+    },
+    isPlainObject: function(obj) {
+      if (obj && Object.prototype.toString.call(obj) === "[object Object]" && obj.constructor === Object && !hasOwnProperty.call(obj, "constructor")) {
+        var key;
+        for (key in obj) {}
+        return key === undefined || hasOwnProperty.call(obj, key);
+      }
+      return false;
+    },
+    extend: function() {
+      var options, name, src, copy, copyIsArray, clone,
+        target = arguments[0] || {},
+        i = 1,
+        length = arguments.length,
+        deep = false;
+      if (typeof target === "boolean") {
+        deep = target;
+        target = arguments[1] || {};
+        i = 2;
+      }
+      if (typeof target !== "object" && !this.isFunction(target)) {
+        target = {};
+      }
+      if (length === i) {
+        target = this;
+        --i;
+      }
+      for (; i < length; i++) {
+        if ((options = arguments[i]) != null) {
+          for (name in options) {
+            src = target[name];
+            copy = options[name];
+            if (target === copy) {
+              continue;
+            }
+            if (deep && copy && (this.isPlainObject(copy) || (copyIsArray = this.isArray(copy)))) {
+              if (copyIsArray) {
+                copyIsArray = false;
+                clone = src && this.isArray(src) ? src : [];
+              } else {
+                clone = src && this.isPlainObject(src) ? src : {};
+              }
+              target[name] = this.extend(deep, clone, copy);
+            } else if (copy !== undefined) {
+              target[name] = copy;
+            }
+          }
+        }
+      }
+      return target;
+    },
+    deepFreeze(obj) {
+      const that = this;
+      Object.freeze(obj);
+      Object.keys(obj).forEach((key, value) => {
+        if (that.isObject(obj[key])) {
+          this.deepFreeze(obj[key]);
+        }
+      });
+      return obj;
+    },
+    deepCopy(data) {
+      let copyOne = null;
+      if (this.isObject(data)) {
+        copyOne = {};
+        for (const key in data) {
+          copyOne[key] = this.deepCopy(data[key]);
+        }
+      } else if (this.isArray(data)) {
+        copyOne = [];
+        for (const index of data) {
+          copyOne.push(this.deepCopy(index));
+        }
+      } else {
+        copyOne = data;
+      }
+      return copyOne;
+    },
+    valueForKeypath(obj, keypath) {
+      let array = null;
+      if (this.isArray(keypath)) {
+        array = keypath;
+      } else if (this.isString(keypath)) {
+        array = keypath.split('.');
+      }
+      if (array === null) {
+        return null;
+      }
+      if (array.length > 1) {
+        const first = array.shift();
+
+        if (!obj[first] || typeof obj[first] !== 'object') {
+          return null;
+        }
+        return this.valueForKeypath(obj[first], array)
+      }
+      return obj[array[0]];
+    },
+    setValueForKeypath(obj, keypath, value) {
+      let array = null;
+      if (this.isArray(keypath)) {
+        array = keypath;
+      } else if (this.isString(keypath)) {
+        array = keypath.split('.');
+      }
+      if (array === null) {
+        return false;
+      }
+      if (array.length > 1) {
+        const first = array.shift();
+
+        if (!obj[first] || typeof obj[first] !== 'object') {
+          obj[first] = {};
+        }
+        this.setValueForKeypath(obj[first], array, value);
+        return false;
+      }
+      obj[array[0]] = value;
+      return true;
+    },
+    toArray(object, keyName, arg3) {
+      let titleName = '';
+      if (!this.isObject(object)) {
+        return [];
+      }
+      if (this.isString(arg3)) {
+        titleName = arg3;
+      }
+      let listO = [];
+      for (let i in object) {
+        let value = object[i];
+        let n = {};
+        if (this.isObject(value)) {
+          n = value;
+        } else {
+          n[titleName] = value;
+        }
+        if (keyName) n[keyName] = key;
+        list.push(n);
+      }
+      return listO;
+    },
+    toObject(list, idName = 'id', hasNum = false) {
+      const listO = {};
+      for (var i = 0; i < list.length; i++) {
+        const n = list[i];
+        if (idName == 'count') {
+          listO[i] = n;
+        } else {
+          listO[n[idName]] = n;
+          if (hasNum) {
+            listO[n[idName]].count = i;
+          }
+        }
+      }
+      return listO;
+    },
+  }
+  return heyUtils;
+}));
