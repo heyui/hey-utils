ce1eb47cb554805a7aab7094f790f984945f5640
vvpvvp <alias8516@sina.com.cn>
2017-04-11 10:31:57 +0800

    uid

--- build/utils.js
+++ build/utils.js
@@ -349,7 +349,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
       var s4 = function s4() {
         return Math.floor((1 + Math.random) * 0x10000).toString(16).substring(1);
       };
-      return s4 + s4 + '-' + s4 + '-' + s4 + '-' + s4 + '-' + s4 + s4 + s4;
+      return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
     }
   };
   return heyUtils;
--- utils.js
+++ utils.js
@@ -315,7 +315,7 @@
       const s4 = ()=>{
         return Math.floor( ( 1 + Math.random ) * 0x10000 ).toString( 16 ).substring( 1 );
       };
-      return s4 + s4 + '-' + s4 + '-' + s4 + '-' + s4 + '-' + s4 + s4 + s4;
+      return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
     }
   }
   return heyUtils;
